c53611dc17bcdb9e92906206f842ec423663b099
Amit Prakash <aprakash@internal.tavisca.com>
2017-12-06 17:20:21 +0530

    some change in insert script

--- FileDataReader/Tavisca.CityTable.Manipulation.Core/ExcelFileManipulation/ScriptGenerator.cs
+++ FileDataReader/Tavisca.CityTable.Manipulation.Core/ExcelFileManipulation/ScriptGenerator.cs
@@ -21,8 +21,11 @@ namespace Tavisca.CityTable.Manipulation.Core.ExcelFileManipulation
 
                     for(int i=1;i<cities.Count;i++)
                     {
-                        writer.WriteLine("Insert into Cities (CityName, StateCode, CountryCode, Latitude, Longitude, IsEnabled, IataCityCode, FullTextColumn) values('" + cities[i].CityName + "' , '" + cities[i].StateCode + "' , '" + cities[i].CountryCode + "' , '" + cities[i].Latitude + "' , '" + cities[i].Longitude + "' , '" + cities[i].IsEnabled + "' , '" + (cities[i].IataCityCode != null ? cities[i].IataCityCode : cities[i].IataCityCode = "NULL") + "' , '" + cities[i].FullTextColumn + "');");
-
+                        writer.WriteLine("IF NOT EXISTS");
+                        writer.WriteLine("(Select * from Cities where CityName = '"+cities[i].CityName.Trim()+"' , '"+ "StateCode = '" + cities[i].StateCode.Trim()+ "' , '"+ "AND CountryCode = '"+cities[i].CountryCode.Trim()+")");
+                        writer.WriteLine("Begin");
+                        writer.WriteLine("Insert into Cities (CityName, StateCode, CountryCode, Latitude, Longitude, IsEnabled, IataCityCode, FullTextColumn) values('" + cities[i].CityName.Trim() + "' , '" + cities[i].StateCode.Trim() + "' , '" + cities[i].CountryCode.Trim() + "' , '" + cities[i].Latitude.Trim() + "' , '" + cities[i].Longitude.Trim() + "' , '" + cities[i].IsEnabled.Trim() + "' , '" + (cities[i].IataCityCode != null ? cities[i].IataCityCode.Trim() : cities[i].IataCityCode = "NULL".Trim()) + "' , '"+cities[i].FullTextColumn.Trim() + "');");
+                        writer.WriteLine("End");
                     }
 
                 }
